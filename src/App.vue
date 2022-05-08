a940ba02953a659a862cd30e1b38b3d7b6370680
José Carlos <etijcvivi@gmail.com>
2022-05-07 22:25:27 -0300

    Criando folhas de estilos com SCSS

--- src/App.vue
+++ src/App.vue
@@ -26,33 +26,5 @@
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2? family= Lato:wght@300;400;700 & family= League+Spartan:wght@500;600;700 & family= Manrope & family= Montserrat:wght@100 & family= Viaduto:wght@400;700 & display=swap');
 
-#app {
-  font-family: Spartan;
-  font-size: 12px;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
-  color: #2c3e50;
-}
-
-
-
-nav {
-  padding: 30px;
-
-  li {
-    list-style: none;
-    display: inline;
-    margin: 10px 10px;
-  }
-
-  a {
-    font-weight: 500;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import './StylesSass/principal.scss';
 </style>
