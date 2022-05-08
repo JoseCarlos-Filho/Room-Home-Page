9bfa88bfcc985f1af5242d9c3c29ce351b648602
José Carlos <etijcvivi@gmail.com>
2022-05-07 22:15:32 -0300

    Criando Views e Montagem da Home Page

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <ul>
+        <li>
+          <h1>room</h1>
+        </li>
+        <li>
+          <router-link :to="{name: 'home'}">home</router-link>
+        </li>
+        <li>
+          <router-link :to="{name: 'shop'}">shop</router-link>
+        </li>
+        <li>
+          <router-link :to="{name: 'about'}">about</router-link>
+        </li>
+        <li>
+          <router-link :to="{name: 'contact'}">contact</router-link>
+        </li>
+      </ul>
     </nav>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2? family= Lato:wght@300;400;700 & family= League+Spartan:wght@500;600;700 & family= Manrope & family= Montserrat:wght@100 & family= Viaduto:wght@400;700 & display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Spartan;
+  font-size: 12px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
 }
+
+
 
 nav {
   padding: 30px;
 
+  li {
+    list-style: none;
+    display: inline;
+    margin: 10px 10px;
+  }
+
   a {
-    font-weight: bold;
+    font-weight: 500;
     color: #2c3e50;
 
     &.router-link-exact-active {
