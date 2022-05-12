f573818dca9f420f7b85b16e282a59c8c02b1cc6
José Carlos <etijcvivi@gmail.com>
2022-05-12 12:30:11 -0300

    Montagem da página Home

--- src/views/Home-page.vue
+++ src/views/Home-page.vue
@@ -1,7 +1,38 @@
 <template>
-    <div class="Home-page">
-        <h1>Home</h1>
-    </div>
+    <main>
+        <section class="container-top">
+            <div>
+                <img src="../assets/desktop-image-hero-1.jpg">
+            </div>
+            <div>
+                <h1>Discovery innovative ways to decorate</h1>
+                <p>We provide unmatched quality, comfort, and style for property
+                owners across the country. Our experts combine from and 
+                function in bringing your vision to life. Create a room in your own
+                style with our collection and make your property a reflection of
+                you and what you love.
+                </p>
+            </div>
+        </section>
+
+        <section class="container-botton">
+            <div>
+                <img src="../assets/image-about-dark.jpg">
+            </div>
+            <div>
+                <h2>ABOUT OUR FURNITURE</h2>
+                <p>Our multifunctional collection blends design and function to suit your individual
+                   taste. Make each room unique, or pick a cohesive theme that best express your
+                   interests and what inspires you. Find the furniture pieces you need, from 
+                   traditional to contemporary styles or anything in between. Product specialists
+                   are available to help you create your dream space.
+                </p>
+            </div>
+            <div>
+                <img src="../assets/image-about-light.jpg">
+            </div>
+        </section>
+    </main>
 </template>
 
 <script>
