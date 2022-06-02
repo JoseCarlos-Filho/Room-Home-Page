ef627830a15ad5926c98408b5b973a7912e78403
José Carlos <etijcvivi@gmail.com>
2022-06-01 21:53:51 -0300

    atualizalção do arquivo vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,3 @@
-// module.exports = {
-//   css: {
-//     loaderOptions: {
-//       scss: {
-//         prependData: `
-//           @import "@/src/StylesSass/principal.scss";
-//         `
-//       }
-//     }
-//   }
-// }
-
-
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
